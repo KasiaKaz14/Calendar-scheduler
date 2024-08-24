f4f62e4c4b97f65c5b9e4538c1b8e8237a0fd4a0
KasiaKaz14 <kasiakazmierczak99@gmail.com>
2024-08-24 22:19:35 +0200

    data.js deleted

--- calendar/src/data.js
+++
@@ -1,7 +0,0 @@
-export const apiKey = "AIzaSyCZFdetiRmg_hniEBnxemmEVz0OxqBP--U";
-export const authDomain = "calendar-scheduler-59025.firebaseapp.com";
-export const projectId = "calendar-scheduler-59025";
-export const storageBucket = "calendar-scheduler-59025.appspot.com";
-export const messagingSenderId = "804975176064";
-export const appId = "1:804975176064:web:00449ea12b17e60b76f965";
-export const measurementId = "G-6LXFVHP9BJ";
